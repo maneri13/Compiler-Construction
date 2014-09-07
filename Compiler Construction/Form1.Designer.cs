82ecd7d7b43a8096864def4059cc4ab7160c57a4
Shahnawaz Ali <mr_shah@live.com>
2014-09-07 22:01:58 +0500

    new files added with .gitignore

---
+++ Compiler Construction/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Compiler_Construction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lexical = new System.Windows.Forms.Button();
+            this.codeBlock = new System.Windows.Forms.RichTextBox();
+            this.outputBlock = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Lexical
+            // 
+            this.Lexical.Location = new System.Drawing.Point(60, 35);
+            this.Lexical.Name = "Lexical";
+            this.Lexical.Size = new System.Drawing.Size(75, 23);
+            this.Lexical.TabIndex = 0;
+            this.Lexical.Text = "Lexical Analysis";
+            this.Lexical.UseVisualStyleBackColor = true;
+            this.Lexical.Click += new System.EventHandler(this.Lexical_Click);
+            // 
+            // codeBlock
+            // 
+            this.codeBlock.Location = new System.Drawing.Point(60, 55);
+            this.codeBlock.Name = "codeBlock";
+            this.codeBlock.Size = new System.Drawing.Size(395, 146);
+            this.codeBlock.TabIndex = 2;
+            this.codeBlock.Text = "";
+            // 
+            // outputBlock
+            // 
+            this.outputBlock.Location = new System.Drawing.Point(60, 239);
+            this.outputBlock.Name = "outputBlock";
+            this.outputBlock.ReadOnly = true;
+            this.outputBlock.Size = new System.Drawing.Size(395, 96);
+            this.outputBlock.TabIndex = 3;
+            this.outputBlock.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 359);
+            this.Controls.Add(this.outputBlock);
+            this.Controls.Add(this.codeBlock);
+            this.Controls.Add(this.Lexical);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Lexical;
+        private System.Windows.Forms.RichTextBox codeBlock;
+        private System.Windows.Forms.RichTextBox outputBlock;
+
+    }
+}
+
---
+++ Compiler Construction/WordBreaker.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* switch breaker on when we encounter char from breaker[]
+ * switch addnext on when two breakers are to be combined as one operator eg. += ==
+ * we use default in all cases when there is only one breaker eg. /n ,' '
+ * */
+
+public class WordBreaker
+{
+    char[] breakers = { ' ', '\n', '<', '>' , '+', '-', '*', '/', '=', '&', '|', '!', '#', '$', ',', ';', ':', '(', ')',
+    '{', '}', '[', ']', '.', '\'', '\"' };
+    //string[] output = new string[100];
+    List<string> output = new List<string>();
+
+	public WordBreaker()
+	{
+
+
+	}
+
+    public List<string> breakString(string myString){
+        output = new List<string>();
+        string temp = "";
+        bool breaker = false, addNext = false, isFloat = false;
+        int index = 0, dump = 0;
+        for (int i = 0; i < myString.Length; i++ )
+        {
+            foreach (char j in breakers)
+            {
+                if (myString[i] == j)
+                {
+                    breaker = true;
+                    switch (myString[i])
+                    {
+                        case '+':
+                            if (myString[i + 1] == '+' || myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '-':
+                            if (myString[i + 1] == '-' || myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '*':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '/':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '>':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '<':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '=':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '&':
+                            if (myString[i + 1] == '&') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '|':
+                            if (myString[i + 1] == '|') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '!':
+                            if (myString[i + 1] == '=') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '#':
+                            if (myString[i + 1] == '#' || myString[i + 1] == '$') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '$':
+                            if (myString[i + 1] == '#') { addNext = true; }
+                            else addNext = false;
+                            break;
+                        case '.':
+                            if(int.TryParse(temp, out dump) && int.TryParse(myString[i+1].ToString(),out dump))
+                            {
+                                breaker = false;
+                            }
+                            else if (int.TryParse(myString[i + 1].ToString(), out dump))
+                            {
+                                isFloat = true;
+                            }
+                            break;
+                        default:
+                            addNext = false;
+                            break;
+
+                    }   // switch end
+                }   // if (i==j) end
+                if (breaker) break;
+            }   // foreach (j)
+            if (!breaker)
+            {
+                temp += myString[i];
+            }
+            else if (breaker)
+            {
+                if (temp != "") { output.Add(temp); }
+                temp = myString[i].ToString();
+                if (isFloat)
+                {
+                    breaker = false;
+                    isFloat = false;
+                    continue;
+                }
+                else if (addNext)
+                {
+                    i++;
+                    temp += myString[i];
+                    index++;
+                    addNext = false;
+                }
+                output.Add(temp);
+                temp = "";
+                index++;
+                breaker = false;
+            }
+        }   // for (i)
+        return output;
+    }
+
+
+}
