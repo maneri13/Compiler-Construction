1fde05b9554017a00bd514b979788b7c1fdef66b
Amin <aminahmedkhan@gmail.com>
2014-09-10 00:46:51 +0500

    "Background color"

--- Compiler Construction/Form1.Designer.cs
+++ Compiler Construction/Form1.Designer.cs
@@ -69,7 +69,10 @@
             // 
             // codeBlock
             // 
+            this.codeBlock.AcceptsTab = true;
+            this.codeBlock.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.codeBlock.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBlock.ForeColor = System.Drawing.Color.OldLace;
             this.codeBlock.Location = new System.Drawing.Point(6, 15);
             this.codeBlock.Name = "codeBlock";
             this.codeBlock.Size = new System.Drawing.Size(648, 96);
@@ -78,15 +81,19 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 12F);
             this.richTextBox1.Location = new System.Drawing.Point(6, 19);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(200, 142);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // tabControl1
             // 
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(12, 27);
@@ -97,23 +104,23 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.MenuBar;
             this.tabPage1.Controls.Add(this.groupBox4);
             this.tabPage1.Controls.Add(this.groupBox2);
             this.tabPage1.Controls.Add(this.groupBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(652, 179);
+            this.tabPage1.Size = new System.Drawing.Size(652, 176);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Lexical Analyzer";
-            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(652, 179);
+            this.tabPage2.Size = new System.Drawing.Size(652, 176);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Syntax Analyzer";
             this.tabPage2.UseVisualStyleBackColor = true;
